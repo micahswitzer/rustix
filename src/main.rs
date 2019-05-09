26aa0393e1f08a9667cdb2740338c993b8c7df61
Micah Switzer <micah.switzer.dev@outlook.com>
2019-05-09 00:09:09 -0500

    Added macros to simplify global invocation

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,7 @@ use core::panic::PanicInfo;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    use core::fmt::Write;
-    vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
-    write!(vga_buffer::WRITER.lock(), ", some numbers: {} {}", 42, 1.337).unwrap();
+    println!("Hello World{}", "!");
 
     loop {}
 }
