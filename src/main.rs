f005eea990e89acfd757736dfaccf793c9b22189
Micah Switzer <micah.switzer.dev@outlook.com>
2019-05-08 23:14:52 -0500

    Added test code for the vga buffer

--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,9 @@ mod vga_buffer;
 
 use core::panic::PanicInfo;
 
-static HELLO: &[u8] = b"Hello World!";
-
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0x0b8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    vga_buffer::print_something();
 
     loop {}
 }
