1cbf8f86a0b7f4be52af0dca985c5af146c32487
Micah Switzer <micah.switzer.dev@outlook.com>
2019-05-16 18:26:29 -0500

    Added a basic timer interrup handler

--- src/lib.rs
+++ src/lib.rs
@@ -16,6 +16,7 @@ pub fn init() {
     gdt::init();
     interrupts::init_idt();
     unsafe { interrupts::PICS.lock().initialize() };
+    x86_64::instructions::interrupts::enable();
 }
 
 pub fn test_runner(tests: &[&dyn Fn()]) {
