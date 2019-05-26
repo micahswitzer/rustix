ba8f00bfc91e64156a2eb40ecd86c8c2543379de
Micah Switzer <micah.switzer.dev@outlook.com>
2019-05-25 20:31:47 -0500

    Updated main method call signature to be compatible with the bootloader

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,13 @@
 #![test_runner(rustix::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
+use bootloader::{BootInfo, entry_point};
 use core::panic::PanicInfo;
 use rustix::println;
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
+entry_point!(kernel_main);
+
+fn kernel_main(boot_info: &'static BootInfo) -> ! {
     println!("Hello World!");
 
     rustix::init();
