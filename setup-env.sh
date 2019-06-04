a0dc3cad66f227c3f3380925e12ceda863f80050
Micah Switzer <micah.switzer.dev@outlook.com>
2019-06-04 11:32:33 -0500

    Updated setup-env to be more intelligent

--- setup-env.sh
+++ setup-env.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
-rustup component add rust-src llvm-tools-preview
-cargo install cargo-xbuild bootimage
+# install rust components
+RUSTUP_COMPONENTS=$(rustup component list --installed)
+[[ $RUSTUP_COMPONENTS != *rust-src* ]] &&
+    rustup component add rust-src
+[[ $RUSTUP_COMPONENTS != *llvm-tools-preview* ]] &&
+    rustup component add llvm-tools-preview
+# install rust binaries
+RUST_BINARIES=$(cargo install --list)
+[[ $RUST_BINARIES != *cargo-xbuild* ]] &&
+    cargo install cargo-xbuild
+[[ $RUST_BINARIES != *bootimage* ]] &&
+    cargo install bootimage
