8f9248c2ce9ff570a4c3b3d5d828cbcfbcca0bfd
Restyled.io <commits@restyled.io>
2019-06-04 16:43:15 +0000

    Restyled by shfmt

--- setup-env.sh
+++ setup-env.sh
@@ -3,15 +3,15 @@
 RUSTUP_COMPONENTS=$(rustup component list --installed)
 if [[ $RUSTUP_COMPONENTS != *rust-src* ]]; then
 	rustup component add rust-src
-fi;
+fi
 if [[ $RUSTUP_COMPONENTS != *llvm-tools-preview* ]]; then
 	rustup component add llvm-tools-preview
-fi;
+fi
 # install rust binaries
 RUST_BINARIES=$(cargo install --list)
 if [[ $RUST_BINARIES != *cargo-xbuild* ]]; then
 	cargo install cargo-xbuild
-fi;
+fi
 if [[ $RUST_BINARIES != *bootimage* ]]; then
 	cargo install bootimage
-fi;
+fi
